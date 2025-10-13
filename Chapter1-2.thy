843fb4c28657f64a0a4c3531c74f04ae3d624f4b
Hadi <haa2005@icloud.com>
2025-10-12 22:40:30 -0400

    Hadi transp_projrel, lift_definition Join, join_commute, unique_cross, rp2_P1b_helper, analytic_rp2

--- Chapter1-2.thy
+++ Chapter1-2.thy
@@ -23,7 +23,7 @@ definition punctured_r_3 where
 definition zvec where "zvec = vector[0::real, 0, 0]"
 
 lemma [simp]:
-  shows "zvec =  vector[0::real, 0, 0]" using zvec_def by auto
+  shows "zvec = vector[0::real, 0, 0]" using zvec_def by auto
 
 definition map_vec where
 "map_vec f g v = vec_lambda (map_fun g f (vec_nth v))"
@@ -32,7 +32,7 @@ functor map_vec
   using eq_id_iff by fastforce+
 
 definition projrel :: "(v3) \<Rightarrow> (v3) \<Rightarrow> bool"
-  where "projrel x y \<longleftrightarrow> (x \<noteq>zvec) \<and>  (y \<noteq> zvec) \<and> (\<exists> t::real . t \<noteq> 0 \<and> x =  t *\<^sub>R  y)" 
+  where "projrel x y \<longleftrightarrow> (x \<noteq> zvec) \<and> (y \<noteq> zvec) \<and> (\<exists>t::real. t \<noteq> 0 \<and> x = t *\<^sub>R y)" 
 
 text\<open>\spike Current definition of projective relation 
 is that there's nonzero constant c such that u = cv. The alternative definition 
@@ -46,7 +46,7 @@ proof
   assume ah1: "projrel u v"
   then obtain t where "t \<noteq> 0 \<and> u =  t *\<^sub>R  v" using projrel_def assms by blast
   then show "(u \<noteq> zvec) \<and> (v \<noteq> zvec) \<and> (u \<times> v) = zvec" 
-    using projrel_def cross_refl assms(1) cross3_def cross_zero_right  by auto 
+    using projrel_def cross_refl assms(1) cross3_def cross_zero_right by auto 
   next
     assume ah2: "(u \<noteq> zvec) \<and> (v \<noteq> zvec) \<and> (u \<times> v) = zvec"
     then have "collinear {0, u, v}" 
@@ -57,7 +57,7 @@ proof
     obtain s where sf: "v - 0 = s *\<^sub>R w" using col_fact by blast
     have uw: "u = r *\<^sub>R w" using rf by simp
     have vw: "v = s *\<^sub>R w" using sf by simp
-    have sr: "s \<noteq> 0 \<and> r \<noteq> 0" using uw vw assms ah2 cross3_def  by fastforce
+    have sr: "s \<noteq> 0 \<and> r \<noteq> 0" using uw vw assms ah2 cross3_def by fastforce
     then have "u = (r/s) *\<^sub>R v" using uw vw by simp
     then have "(\<exists> t::real . (t \<noteq>0) \<and> u =  t *\<^sub>R  v)" using sr  divide_eq_0_iff by blast
     then show "projrel u v" using projrel_def ah2 assms by blast
@@ -66,8 +66,8 @@ proof
 text \<open>\done\<close>
 
 lemma vt:
-  shows "(vector[1,0,0]::real^3) \<noteq> zvec" using zvec_def
-  by (metis vector_3(1) zero_neq_one)
+  shows "(vector[1,0,0]::real^3) \<noteq> zvec" 
+  using zvec_def vector_3(1) zero_neq_one by metis
 
 lemma exists_projrel_refl: "\<exists>x. projrel x x" 
 proof -
@@ -76,18 +76,19 @@ proof -
 qed
 
 lemma symp_projrel: "symp projrel"
-proof -
-  show ?thesis  unfolding symp_def projrel_def 
-    using cross_mult_right projrel_def by (metis divideR_right scaleR_zero_left)
-qed
+  using divideR_right scaleR_zero_left cross_mult_right projrel_def 
+  unfolding symp_def projrel_def by metis
 
+text \<open>\hadi\<close>
 lemma transp_projrel: "transp projrel"
 proof (rule transpI)
   fix x y z
   assume 1: "projrel x y"
   assume 2: "projrel y z"
-  show  "projrel x z" sorry
+  show "projrel x z" using 1 2 alt_projrel cross_mult_left 
+    cross_zero_right projrel_def by (metis (lifting) ext)
 qed
+text \<open>\done\<close>
 
 lemma part_equivp_projrel: "part_equivp projrel"
   by (rule part_equivpI [OF exists_projrel_refl symp_projrel transp_projrel])
@@ -100,7 +101,7 @@ lemma smult_projrel:
   assumes c_def: "c \<noteq> 0"
   assumes smult: "x = c *\<^sub>R y"
   shows "projrel x y"
-  using c_def projrel_def smult x_def y_def  by blast
+  using c_def projrel_def smult x_def y_def by blast
 text \<open>\done\<close>
 
 quotient_type rp2 = "v3" / partial: "projrel"
@@ -110,28 +111,30 @@ quotient_type rp2 = "v3" / partial: "projrel"
 find_theorems name: "rp2"
 find_theorems name: "Quotient3_rp2"
 
-lemma Domainp_cr_proj [transfer_domain_rule]: "Domainp cr_rp2 = (\<lambda>x .( (x \<noteq> zvec) \<and> projrel x x))"
+lemma Domainp_cr_proj [transfer_domain_rule]: "Domainp cr_rp2 = (\<lambda>x .((x \<noteq> zvec) \<and> projrel x x))"
 proof -
   have "projrel x x \<longrightarrow> x  \<noteq> zvec" for x using projrel_def by blast
-  then show ?thesis  using projrel_def rp2.domain by auto 
+  then show ?thesis using projrel_def rp2.domain by auto 
 qed
 
 lemma rep_P_nz:
   fixes P
   assumes a1: "P \<in> rp2_Points" 
   shows "Rep_Proj P \<noteq> zvec" 
-using projrel_def Quotient_rel_rep Quotient_rp2 by metis
+  using projrel_def Quotient_rel_rep Quotient_rp2 by metis
 
 (* a remaining theorem from the "warmup" section, one that needs "projrel", and
 needs rewriting using Cross3 rather than our (now-delete) version of 'cross' *)
 
+text \<open>\hadi\<close>
 lemma cross_nz:
   assumes "u \<noteq> zvec"
   assumes "v \<noteq> zvec"
   assumes "\<not> projrel u v"
   defines s_def: "s \<equiv> u \<times> v"
-  shows "s  \<noteq> zvec" 
-  using assms(1,2,3) cross3_def projrel_def  s_def alt_projrel by metis
+  shows "s \<noteq> zvec" 
+  using assms cross3_def projrel_def s_def alt_projrel by metis
+text \<open>\done\<close>
 
 (* We've defined RP2, but we still need to show it's a projective plane, i.e., demonstrate 
 axioms 1 - 4. Then we can move on to isomorphism with the completion of the affine plane. *)
@@ -149,19 +152,19 @@ definition rp2_incid_rep where
 
 text \<open>\hadi\<close>
 lift_definition rp2_incid::"rp2 \<Rightarrow> rp2 \<Rightarrow> bool"
-is "\<lambda> P k . (P \<bullet> k = 0)"
+is "\<lambda>P k. (P \<bullet> k = 0)"
 proof -
   fix P1 P2 k1 k2
   assume a1: "projrel P1 P2"
   assume a2: "projrel k1 k2"
   obtain s where P12: "s \<noteq> 0 \<and> P1 = s *\<^sub>R P2" 
-    using alt_projrel [of P1 P2]  a1 projrel_def [of P1 P2] cross3_def by fastforce
+    using a1 alt_projrel [of P1] projrel_def [of P1] cross3_def by fastforce
   obtain t where k12: "t \<noteq> 0 \<and> k1 = t *\<^sub>R k2"
-    using alt_projrel [of k1 k2]  a2 projrel_def [of k1 k2]  cross3_def by fastforce
-  have ts: "t \<noteq> 0 \<and> s \<noteq> 0"  using P12 k12 by auto
+    using a2 alt_projrel [of k1] projrel_def [of k1] cross3_def by fastforce
+  have ts: "t \<noteq> 0 \<and> s \<noteq> 0" using P12 k12 by auto
   have "(P1 \<bullet> k1 = 0) = (P1 \<bullet> (t *\<^sub>R k2) = 0)" using k12 by auto 
   also have "... = ( P2 \<bullet>  k2 = 0)" using P12 ts by auto
-  finally show "(P1 \<bullet> k1 = 0) =(P2 \<bullet> k2 = 0)" .
+  finally show "(P1 \<bullet> k1 = 0) = (P2 \<bullet> k2 = 0)" .
 qed
 
 lemma incid_commute:
@@ -176,46 +179,89 @@ text \<open>\done\<close>
 *)
 
 definition join :: "real^3 \<Rightarrow> real^3 \<Rightarrow> real^3"
-  where
-  "join P Q = (if P \<times> Q = zvec then vector[0,0,1] else P \<times> Q)"
+  where "join P Q = (if P \<times> Q = zvec then vector[0,0,1] else P \<times> Q)"
 
-lift_definition Join :: "rp2 \<Rightarrow> rp2 \<Rightarrow> rp2"
-  is "\<lambda>P Q. join P Q" 
-sorry 
+text \<open>\hadi\<close>
+lift_definition Join :: "rp2 \<Rightarrow> rp2 \<Rightarrow> rp2" is "\<lambda>P Q. join P Q" 
+proof -
+  fix v1 v2 v3 v4
+  assume a1: "projrel v1 v2"
+  assume a2: "projrel v3 v4"
+  show "projrel (join v1 v3) (join v2 v4)" 
+  proof -
+    have nz: "(join v1 v3) \<noteq> zvec \<and> (join v2 v4) \<noteq> zvec" 
+      using join_def zvec_def vector_3(3) rel_simps(93) by metis
+    obtain t1 t2 where t1t2def: "t1 \<noteq> 0 \<and> v1 = t1 *\<^sub>R v2 
+      \<and> t2 \<noteq> 0 \<and> v3 = t2 *\<^sub>R v4" using a1 a2 projrel_def by auto
+    consider
+    (allnz) "v1 \<times> v3 \<noteq> zvec \<and> v2 \<times> v4 \<noteq> zvec"
+    | (onez) "(v1 \<times> v3 = zvec \<and> v2 \<times> v4 \<noteq> zvec) \<or> (v1 \<times> v3 \<noteq> zvec \<and> v2 \<times> v4 = zvec)"
+    | (allz) "v1 \<times> v3 = zvec \<and> v2 \<times> v4 = zvec" by blast
+    then show ?thesis 
+    proof (cases)
+      case allnz
+      then show ?thesis using t1t2def join_def alt_projrel projrel_def cross_refl
+        cross_mult_left cross_mult_right scaleR_zero_right by (metis (no_types))
+    next
+      case onez
+      then have "(join v1 v3) \<times> (join v2 v4) = zvec" using a1 a2 join_def
+        alt_projrel projrel_def part_equivp_def part_equivp_projrel by metis
+      then show ?thesis using nz alt_projrel by auto
+    next
+      case allz
+      then show ?thesis using nz join_def projrel_def by auto
+    qed
+  qed
+qed
+text \<open>\done\<close>
 
+text \<open>\hadi\<close>
 lemma join_commute:
   fixes P Q
   assumes a1: "P \<in> rp2_Points" 
   assumes a2: "Q \<in> rp2_Points"
   assumes a3: "P \<noteq> Q"
   shows "Join P Q = Join Q P"
-sorry
-(* use "proof (transfer)" to get started here *)
+proof (transfer)
+  fix Ps Qs
+  assume b1: "Ps \<noteq> zvec \<and> projrel Ps Ps"
+  assume b2: "Qs \<noteq> zvec \<and> projrel Qs Qs"
+  show "projrel (join Ps Qs) (join Qs Ps)"
+  proof -
+    have nz: "(join Ps Qs) \<noteq> zvec \<and> (join Qs Ps) \<noteq> zvec" 
+      using join_def zvec_def vector_3(3) rel_simps(93) by metis
+    have "\<exists>(t::real). t \<noteq> 0 \<and> (join Ps Qs) = t *\<^sub>R (join Qs Ps)"
+      using b2 alt_projrel cross_skew join_def pth_1 scaleR_left.minus 
+      scaleR_zero_right zero_neq_one by (metis (no_types))
+    then show ?thesis using nz projrel_def by auto
+  qed
+qed
+text \<open>\done\<close>
 
 text \<open>\hadi\<close>
-
 lemma rp2_P1a:
   fixes P Q
   assumes a1: "P \<in> rp2_Points" 
   assumes a2: "Q \<in> rp2_Points"
-(*  assumes a3: "P \<noteq> Q" *)
-  shows "(\<exists>k . rp2_incid P  k  \<and>  rp2_incid Q  k)"
+  shows "(\<exists>k. rp2_incid P k \<and> rp2_incid Q k)"
 proof (transfer)
   fix P Q 
-  show "P \<noteq> zvec \<and> projrel P P \<Longrightarrow> Q \<noteq> zvec \<and> projrel Q Q \<Longrightarrow> \<exists>k\<in>{x. x \<noteq> zvec \<and> projrel x x}. P \<bullet> k = 0 \<and> Q \<bullet> k = 0"
+  show "P \<noteq> zvec \<and> projrel P P \<Longrightarrow> Q \<noteq> zvec \<and> projrel Q Q 
+    \<Longrightarrow> \<exists>k \<in> {x. x \<noteq> zvec \<and> projrel x x}. (P \<bullet> k = 0) \<and> (Q \<bullet> k = 0)"
   proof -
     assume pf: "P \<noteq> zvec \<and> projrel P P"
     assume qf: "Q \<noteq> zvec \<and> projrel Q Q"
-    show "\<exists>k\<in>{x. x \<noteq> zvec \<and> projrel x x}. P \<bullet> k = 0 \<and> Q \<bullet> k = 0"
+    show "\<exists>k \<in> {x. x \<noteq> zvec \<and> projrel x x}. (P \<bullet> k = 0) \<and> (Q \<bullet> k = 0)"
     proof (cases "projrel P Q")
-      case 1: True
-      obtain u where "P \<times> u \<noteq> 0" using cross_basis_nonzero[of P] by (metis alt_projrel pf)
+      case pqrel: True
+      obtain u where udef: "P \<times> u \<noteq> 0" 
+        using cross_basis_nonzero [of P] alt_projrel pf by metis
       then have "P \<bullet> (P \<times> u) = 0 \<and> Q \<bullet> (P \<times> u) = 0"
-       using 1 cross_mult_left dot_cross_self(1) projrel_def by auto
-      then show ?thesis using \<open>P \<times> u \<noteq> 0\<close> alt_projrel pf by auto
+        using pqrel cross_mult_left projrel_def dot_cross_self(1) by auto
+      then show ?thesis using udef alt_projrel pf by auto
     next
-      case 2: False
-      then show ?thesis  using alt_projrel dot_cross_self(1,2) pf qf by auto
+      case False
+      then show ?thesis using alt_projrel dot_cross_self(1,2) pf qf by auto
     qed
   qed
 qed
@@ -225,8 +271,9 @@ lemma unique_cross:
   fixes a b n k
   assumes "a \<times> b \<noteq> 0"
   assumes "k \<bullet> a = 0" and "k \<bullet> b = 0" 
-  shows "\<exists> s . k = s *\<^sub>R (a \<times> b)"
-sorry
+  shows "\<exists>s. k = s *\<^sub>R (a \<times> b)"
+  using assms Lagrange zvec_def projrel_def cross3_def cross_nz scaleR_zero_left
+    cancel_comm_monoid_add_class.diff_cancel zero_index by (metis (no_types))
 
 (* TO DO: To show uniqueness of the join, we have to show (for P,Q nonzero and P and Q not proj_rel,
 that if h is orthog to P and Q, then h is a nonzero multiple of the cross product, i.e., the lemma
@@ -239,8 +286,9 @@ lemma rp2_P1b_helper:
   assumes n_def: "n = P \<times> Q"
   assumes k_def: "k \<noteq> zvec"
   assumes k_facts: "(P \<bullet> k = 0)  \<and> (Q \<bullet> k = 0)" 
-  shows "\<exists>c . (c \<noteq> 0) \<and> k = c *\<^sub>R n"
-  sorry
+  shows "\<exists>c. (c \<noteq> 0) \<and> k = c *\<^sub>R n"
+  using assms alt_projrel cross_refl exists_projrel_refl inner_commute 
+    projrel_def scaleR_zero_left unique_cross by metis
 
 text \<open>\nick\jackson\<close>
 lemma rp2_P1b:
@@ -274,9 +322,7 @@ proof -
   obtain c_n where hcn: "(c_n \<noteq> 0) \<and> nvec = c_n *\<^sub>R ?crossPQ"
     using a1 a2 a3 h1 h3 hPrep hQrep hkrep hPpr3 hQpr3 hnpr3 rep_P_nz rp2_P1b_helper[of Pvec Qvec ?crossPQ nvec] by auto
   have h4: "((c_k/c_n) \<noteq> 0) \<and> kvec = (c_k/c_n) *\<^sub>R nvec" using hck hcn by auto
-  show ?thesis 
-    using h4 Quotient3_rel_rep Quotient3_rp2 hkrep hnrep projrel_def hkpr3 hnpr3 smult_projrel
-    by metis
+  show ?thesis using h4 Quotient3_rel_rep Quotient3_rp2 hkrep hnrep projrel_def hkpr3 hnpr3 smult_projrel by metis
 qed
 text \<open>\done\<close>
 
@@ -285,7 +331,7 @@ lemma rp2_P2:
   assumes a1: "m \<in> rp2_Lines" 
   assumes a2: "k \<in> rp2_Lines"
   assumes a3: "m \<noteq> k"
-  shows "(\<exists>P . P \<in> rp2_Points \<and> rp2_incid P  m  \<and>  rp2_incid P  k)"
+  shows "(\<exists>P . P \<in> rp2_Points \<and> rp2_incid P m \<and> rp2_incid P k)"
   sorry
 
 lemma rp2_P3:
@@ -302,7 +348,7 @@ lemma rp2_P4:
   assumes "U = {X . X \<in> rp2_Points \<and> rp2_incid X k}"
   shows "\<exists>P Q R. P \<in> U \<and> Q \<in> U \<and> R \<in> U \<and> distinct [P,Q,R]"
 proof - 
-  obtain P Q where pq: "P \<in> U \<and> Q \<in> U" using assms 
+  obtain P Q where pq: "P \<in> U \<and> Q \<in> U" using assms rp2_Lines_def rp2_Points_def
       mem_Collect_eq rp2_P1a rp2_P2 rp2_P3 by metis
   obtain a b c :: real where abc: "Rep_Proj P = vector[a, b, c]" 
     using forall_vector_3 by fastforce
@@ -314,8 +360,7 @@ proof -
   have 0: "Abs_Proj (vector[g, h, i]) \<in> rp2_Points" using rp2_Points_def by auto 
 
   obtain R where r: " R = Abs_Proj (vector[g, h, i])" using 0 by auto
-  then have r_rep: "projrel((Rep_Proj R)) (vector[g, h, i])" using ra
-    sorry
+  then have r_rep: "projrel((Rep_Proj R)) (vector[g, h, i])" sorry
   then have 0: "distinct[P,Q,R]" using num1_eq1 vt sorry
   then have 1: "R \<in> U" using num1_eq1 vt sorry
   then show ?thesis using pq r 0 1 by auto
@@ -323,9 +368,24 @@ qed
 
 text \<open>\done\done\<close>
 
+text \<open>\hadi\<close>
 theorem analytic_rp2:
   shows "projective_plane2 rp2_Points rp2_Lines rp2_incid"
-  sorry
+proof (unfold_locales)
+  show "\<lbrakk>P \<noteq> Q; P \<in> rp2_Points; Q \<in> rp2_Points\<rbrakk> 
+    \<Longrightarrow> (\<exists>!k. k \<in> rp2_Lines \<and> rp2_incid P k \<and> rp2_incid Q k)" for P Q 
+    using rp2_P1a rp2_P1b rp2_Lines_def UNIV_I by metis
+  show "\<lbrakk>k \<in> rp2_Lines; n \<in> rp2_Lines\<rbrakk> 
+    \<Longrightarrow> \<exists>P. (P \<in> rp2_Points \<and> rp2_incid P k \<and> rp2_incid P n)" for k n
+    using rp2_P2 rp2_P3 rp2_Points_def rp2_Lines_def by metis
+  show "\<exists>P Q R. P \<in> rp2_Points \<and> Q \<in> rp2_Points \<and> R \<in> rp2_Points  \<and> P \<noteq> Q \<and> P \<noteq> R \<and> Q \<noteq> R 
+    \<and> \<not> (projective_plane_data2.pcollinear rp2_Points rp2_Lines rp2_incid P Q R)"
+    using rp2_P3 rp2_Points_def rp2_Lines_def by (simp add: projective_plane_data2.pcollinear_def)
+  show "\<lbrakk>k \<in> rp2_Lines; U = {P. (P \<in> rp2_Points \<and> rp2_incid P k)}\<rbrakk> 
+    \<Longrightarrow> \<exists>Q R S. Q \<in> U \<and> R \<in> U \<and> S \<in> U \<and> distinct [Q, R, S]" for k U 
+    using rp2_P4 by auto
+qed
+text \<open>\done\<close>
 
 (* also needed: an interpretation claim like those for A4 and A2 *)
 interpretation RP2Q: projective_plane2 rp2_Points rp2_Lines rp2_incid
@@ -337,7 +397,7 @@ theorem projectivisation_of_A2:
                   ((k \<in> A2Lines) \<and> (t = affine_plane_data.line_pencil  A2Points  A2Lines ( a2incid) k) )}"
   defines pLdef: "pLines \<equiv> {OrdinaryL n | n . (n \<in>  A2Lines)} \<union> {Infty}"
   defines pm: "pincid \<equiv>  mprojectivize (a2incid)"
-  shows   "projective_plane2 pPoints pLines pincid"
+  shows "projective_plane2 pPoints pLines pincid"
   using "Chapter1-1.projectivization_is_projective" A2_affine assms(1,2,3) by blast
 
 interpretation RP2C: projective_plane2  "{OrdinaryP P | P . (P \<in> A2Points)} \<union> {Ideal t | k t . 
@@ -348,5 +408,3 @@ interpretation RP2C: projective_plane2  "{OrdinaryP P | P . (P \<in> A2Points)} 
 place these Chapter 1-3. *)
 
 end
-
-
--- Chapter4-1.thy
+++ Chapter4-1.thy
@@ -1,10 +1,23 @@
 theory "Chapter4-1"
-  imports Complex_Main  "Chapter1-1"
+  imports Complex_Main "Chapter1-1"
 
 begin
 text\<open> Everything up to the Principle of Duality and the remarks after it. (You don't need to prove 
 remark 2!)\<close>
 
+definition (in projective_plane2) proj_point_pencil :: "'p \<Rightarrow> 'l set" where
+  "proj_point_pencil (P::'p) = {n::'l . (n \<in> Lines) \<and> incid P n}"
+
+lemma dual_plane_is_projective:
+  fixes Points :: "'p set"
+  fixes Lines :: "'l set"
+  fixes incid :: "'p \<Rightarrow> 'l \<Rightarrow> bool"
+  assumes pp: "projective_plane2 Points Lines incid"
+  defines dPoints: "dPoints \<equiv> Lines"
+  defines dLines: 
+    "dLines \<equiv> {projective_plane2.proj_point_pencil P::'p | P. (P \<in> Points)}"
+  shows "P = 1"
+ 
 end
 
 
