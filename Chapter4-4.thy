1dfdcfe337be2dc3fbf56b962b03cabbe960282b
Nicholas Jankovic <123640069+nickjankovic@users.noreply.github.com>
2025-10-24 16:35:54 -0400

    prop 4.8

--- Chapter4-4.thy
+++ Chapter4-4.thy
@@ -3,15 +3,15 @@ theory "Chapter4-4"
 begin
 text\<open> start at "Perspectivies and Projectivities" and go to end of chapter\<close>
 
-definition (in projective_plane_data2) isperspectivity :: "'p \<Rightarrow> 'l \<Rightarrow> 'l \<Rightarrow> bool" 
+definition (in projective_plane) isperspectivity :: "'p \<Rightarrow> 'l \<Rightarrow> 'l \<Rightarrow> bool" 
   where "isperspectivity Or l1 l2 = (if Or \<in> Points \<and> l1 \<in> Lines \<and> l2 \<in> Lines 
   then (\<not> (incid Or l1) \<and> \<not> (incid Or l2)) else undefined)"
 
-definition (in projective_plane_data2) perspectivity :: "'p \<Rightarrow> 'l \<Rightarrow> 'l \<Rightarrow> ('p \<Rightarrow> 'p)"
+definition (in projective_plane) perspectivity :: "'p \<Rightarrow> 'l \<Rightarrow> 'l \<Rightarrow> ('p \<Rightarrow> 'p)"
   where "perspectivity Or l1 l2 = (if (Or \<in> Points \<and> l1 \<in> Lines \<and> l2 \<in> Lines \<and> isperspectivity Or l1 l2)
   then (\<lambda>Q . if Q \<in> Points \<and> incid Q l1 then (meet (join Or Q) l2) else undefined) else undefined)"
 
-lemma (in projective_plane_data2) perspectivity_inj:
+lemma (in projective_plane) perspectivity_inj:
   fixes f Or l1 l2 P Q
   assumes l1_def: "l1 \<in> Lines"
   assumes l2_def: "l2 \<in> Lines"
@@ -23,7 +23,7 @@ lemma (in projective_plane_data2) perspectivity_inj:
   shows "f P \<noteq> f Q"
   sorry
 
-lemma (in projective_plane_data2) perspectivity_surj:
+lemma (in projective_plane) perspectivity_surj:
   fixes f Or l1 l2 Q
   assumes l1_def: "l1 \<in> Lines"
   assumes l2_def: "l2 \<in> Lines"
@@ -33,7 +33,7 @@ lemma (in projective_plane_data2) perspectivity_surj:
   shows "\<exists> P . P \<in> Points \<and> P \<lhd> l1 \<and> f P = Q"
   sorry
 
-lemma (in projective_plane_data2) inv_is_perspectivity:
+lemma (in projective_plane) inv_is_perspectivity:
   fixes f Or l1 l2 f_inv P
   assumes Or_def: "Or \<in> Points"
   assumes l1_def: "l1 \<in> Lines"
@@ -44,7 +44,7 @@ lemma (in projective_plane_data2) inv_is_perspectivity:
   shows "f_inv = perspectivity Or l2 l1"
   sorry
 
-lemma (in projective_plane_data2) perspectivity_of_meet_is_itself:
+lemma (in projective_plane) perspectivity_of_meet_is_itself:
   fixes f Or l1 l2 P
   assumes Or_def: "Or \<in> Points"
   assumes l1_def: "l1 \<in> Lines"
@@ -55,9 +55,10 @@ lemma (in projective_plane_data2) perspectivity_of_meet_is_itself:
   shows "f P = P"
   sorry
 
-(* Definition. A projectivity is a mapping of one line l into another l\<Zprime> (which may be equal to l), which can be expressed as a composition of perspectivities. We write l Z l\<Zprime>, and write ABC . . . Z A\<Zprime>B\<Zprime>C\<Zprime> . . . if the projectivity that takes  points A, B, C, . . . into A\<Zprime>, B\<Zprime>, C\<Zprime>, . . . respectively. Note that a projectivity also is always one-to-one and onto.   *)
-
-fun (in projective_plane_data2) projectivity :: "'p list \<Rightarrow> 'l list \<Rightarrow> ('p \<Rightarrow> 'p)" where
+(* Definition. A projectivity is a mapping of one line l into another l\<Zprime> (which may be equal to l), which can be expressed as a composition of perspectivities. 
+We write l Z l\<Zprime>, and write ABC . . . Z A\<Zprime>B\<Zprime>C\<Zprime> . . . if the projectivity that takes  points A, B, C, . . . into A\<Zprime>, B\<Zprime>, C\<Zprime>, . . . respectively. 
+Note that a projectivity also is always one-to-one and onto. *)
+fun (in projective_plane) projectivity :: "'p list \<Rightarrow> 'l list \<Rightarrow> ('p \<Rightarrow> 'p)" where
   "projectivity (Cons p []) (Cons l1 (Cons l2 [])) = (perspectivity p l1 l2)" |
   "projectivity (Cons p ps) (Cons l1 (Cons l2 ls)) = (projectivity ps (Cons l2 ls)) \<circ> (perspectivity p l1 l2)" |
   "projectivity [] b = undefined" |
@@ -65,15 +66,23 @@ fun (in projective_plane_data2) projectivity :: "'p list \<Rightarrow> 'l list \
   "projectivity a [v] = undefined"
 
 (* Proposition 4.8 Let l be a line. Then the set of projectivities of l into itself forms a group, which we will call PJ(l). *)
+lemma (in projective_plane) PJ_l_is_group:
+  fixes l PJ_l
+  assumes l_def: "l \<in> Lines"
+  assumes PJ_l_def: "PJ_l = {f . \<exists> ps . \<exists> ls . (f = projectivity ps ls) \<and> (hd ls = l) \<and> (last ls = l)}"
+  shows True
+  sorry
 
-(* Proposition 4.9 Let l be a line, and let A, B, C, and A\<Zprime>, B\<Zprime>, C\<Zprime> be two triples of three distinct points each on l. Then there is a projectivity of l into itself which sends A, B, C into A\<Zprime>, B\<Zprime>, C\<Zprime>. *)
-
-lemma (in projective_plane_data2) exists_projectivity_triplet_to_triplet:
+(* Proposition 4.9 Let l be a line, and let A, B, C, and A\<Zprime>, B\<Zprime>, C\<Zprime> be two triples of three distinct points each on l. 
+Then there is a projectivity of l into itself which sends A, B, C into A\<Zprime>, B\<Zprime>, C\<Zprime>. *)
+lemma (in projective_plane) exists_projectivity_triplet_to_triplet:
   fixes A B C A' B' C' l
   assumes ABC_def: "A \<in> Points \<and> B \<in> Points \<and> C \<in> Points \<and> distinct [A, B, C]"
   assumes ABC'_def: "A' \<in> Points \<and> B' \<in> Points \<and> C' \<in> Points \<and> distinct [A', B', C']"
   assumes l_def: "l \<in> Lines \<and> A \<lhd> l \<and> B \<lhd> l \<and> C \<lhd> l \<and> A' \<lhd> l \<and> B' \<lhd> l \<and> C' \<lhd> l"
-  shows "\<exists> ps . \<exists> ls . f = projectivity ps (App (Cons l ls) [l]) \<and> f A = A' \<and> f B = B' \<and> f C = C'"
+  shows "\<exists> ps . \<exists> ls . (f = projectivity ps ls) 
+                        \<and> (hd ls = l) \<and> (last ls = l) 
+                        \<and> (f A = A') \<and> (f B = B') \<and> (f C = C')"
   sorry
 
 (* Proposition 4.10 A projectivity takes harmonic quadruples into harmonic quadruples. *)
