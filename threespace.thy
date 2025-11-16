7fe9d4ad9ae35bf52e42e14351011c78eaade2f2
rshlyakh <robert.shlyakhtenko@icloud.com>
2025-11-16 13:19:23 -0800

    vanadium

--- threespace.thy
+++ threespace.thy
@@ -81,6 +81,13 @@ assumes
     S0b: "\<lbrakk>H \<in> Planes; P \<in> H\<rbrakk> \<Longrightarrow> P \<in> Points"
 begin
 
+lemma S2_exists_unique:
+  fixes P Q R
+  assumes "P \<in> Points \<and> Q \<in> Points \<and> R \<in> Points"
+  assumes "\<not> collinear P Q R"
+  shows "\<exists>! H . H \<in> Planes \<and> P \<in> H \<and> Q \<in> H \<and> R \<in> H" 
+  using assms S2a S2b by blast
+
 text \<open>\hadi\<close>
 lemma S5_dist:
   fixes P Q R S
@@ -138,6 +145,127 @@ proof -
   have "H \<inter> K = k" using empty_diff kdef by auto
   then show ?thesis using kdef by auto
 qed
+
+
+lemma S5alt:
+  shows "\<exists> A B C D . (A \<in> Points \<and> B \<in> Points \<and> C \<in> Points \<and> D \<in>  Points \<and>                                   
+(\<not> coplanar A B C D) \<and> 
+(\<not> collinear A B C) \<and> 
+(\<not> collinear A B D) \<and> 
+(\<not> collinear A C D) \<and> 
+(\<not> collinear B C D))" using S5 coplanar_def collinear_def by metis
+
+lemma collinear_join [iff] :
+  fixes P Q X
+  assumes "P \<in> Points \<and> Q \<in> Points \<and> X \<in> Points"
+  assumes "P \<noteq> Q"
+  shows "collinear P Q X \<longleftrightarrow> X \<in> join P Q"
+  using assms collinear_def S1a S1b by fastforce
+
+
+lemma distinct3_alt [simp]:
+  "distinct[A,B,C] \<longleftrightarrow> (A \<noteq> B \<and> A \<noteq> C \<and> B \<noteq> C)" 
+  by auto
+
+lemma fargle:
+  fixes S T
+  assumes "S \<noteq> T"
+  assumes "S \<in> Planes \<and> T \<in> Planes"
+  shows "\<exists> k . (k \<in> Lines \<and> k = (S \<inter> T))"
+proof -
+  obtain k where kfacts: "(k \<in> Lines \<and> k \<subseteq> (S \<inter> T))" using assms S4[of S T] by auto
+  obtain  P Q where pqfacts: "P\<in> Points \<and> Q \<in> Points \<and> P \<in> k \<and> Q \<in> k \<and> P \<noteq> Q" using kfacts S6[of k] S0a
+  by (metis distinct3_def)
+  have u: "P \<in> S \<and> Q \<in> S \<and> P \<in>T \<and> Q\<in> T" using kfacts pqfacts by auto
+  fix X 
+  have  a0: "X \<in> S\<inter>T \<Longrightarrow> collinear P Q X" for X
+  proof (rule ccontr) (* classical contradiction *)
+    assume ch: "\<not>collinear P Q X"
+    assume minor: "X \<in> S \<inter> T"
+    have d: "distinct[P,Q,X]" using pqfacts ch collinear_def kfacts by force
+    have b: "\<exists>! H . H \<in> Planes \<and> P \<in> H \<and> Q \<in> H \<and> X \<in> H" using S2a[of P Q X] S2b[of P Q X] d ch distinct3_alt[of P Q X] 
+    by (metis Int_iff S0b assms(2) minor u)
+    then have "S = T" using assms u ch b minor by auto 
+    then show False using assms by auto
+  qed
+  have a1: "X \<in> S\<inter>T \<Longrightarrow> X \<in> join P Q" using a0 pqfacts
+  using S0b assms(2) by auto
+  then have a2: "S \<inter> T = join P Q" using S1b a0 kfacts pqfacts
+  by (meson IntI assms(1,2) crossing_planes u)
+  then show ?thesis by (simp add: S1a pqfacts)
+qed
+
+lemma same_joins:
+  fixes a b x y
+  assumes "a \<in> Points \<and> b \<in> Points \<and> x \<in> Points \<and> y \<in> Points"
+  assumes "x \<in> join a b \<and> y \<in> join a b"
+  assumes "distinct4 a b x y"
+  shows "join x y = join a b"
+  using assms
+  by (metis S1a S1b distinct4_def)
+
+lemma vanadium1:
+  fixes a b L
+  assumes "a \<in> Points \<and> b \<in> Points \<and> L \<in> Lines"
+  assumes "a \<noteq> b"
+  assumes "a \<in> L \<and> b \<in> L"
+  shows "L = join a b"
+  using assms S1b by auto
+
+lemma vanadium2:
+  fixes a b c P Q 
+  assumes "a \<in> Points \<and> b \<in> Points\<and> c \<in> Points \<and> P \<in> Planes \<and> Q \<in> Planes"
+  assumes "\<not> collinear a b c" 
+  assumes "a \<in> P \<and> b \<in> P \<and> c \<in> P" 
+  assumes "a \<in> Q \<and> b \<in> Q \<and> c \<in> Q" 
+  assumes "distinct [a, b, c]" 
+  shows "P = Q"
+  using S2a S2b assms collinear_join
+  by (simp; metis)
+
+lemma vanadium3:
+  fixes x y P Q
+  assumes "x \<in> Points \<and> y \<in> Points \<and> P \<in> Planes \<and> Q \<in> Planes"
+  assumes "x \<in> P \<and> x \<in> Q \<and> y \<in> P \<and> y \<in> Q"
+  assumes "x \<noteq> y"
+  shows "P = Q \<or> P \<inter> Q = join x y"
+proof -
+  obtain L where L_facts: "L \<in> Lines \<and> L \<subseteq> P \<and> L \<subseteq> Q" using S4 assms by metis
+  then obtain a b where h: "a \<in> Points \<and> b \<in> Points \<and> a \<noteq> b \<and> a \<in> L \<and> b \<in> L" using S6 S0a
+    by (metis distinct3_def)
+  then have h1: "L = join a b" using L_facts S1b by auto
+  show ?thesis
+  proof (cases "\<exists> p . (p \<in> Points \<and> p \<notin> L \<and> p \<in> P \<and> p \<in> Q)")
+    case True
+    then obtain p where p_facts: "(p \<in> Points \<and> p \<notin> L \<and> p \<in> P \<and> p \<in> Q)" by auto
+    then have "distinct [a, b, p]" using h by auto
+    then have "P = Q" using vanadium2 p_facts h1 L_facts S2_exists_unique assms 
+      by (smt (verit, del_insts) collinear_join h subset_iff)
+    then show ?thesis by auto
+  next
+    case False
+    then have "p \<in> Points \<and> p \<in> P \<and> p \<in> Q \<Longrightarrow> p \<in> L" for p by auto
+    then have h2: "P \<inter> Q = L" using crossing_planes L_facts S0b assms(1) by auto
+    then have "join a b = join x y" using h1 same_joins assms
+    by (metis False L_facts S1b)
+    then show ?thesis using h1 h2 by auto
+  qed
+qed
+
+lemma vanadium4:
+  fixes x y z w
+  assumes "x \<in> Points \<and> y \<in> Points \<and> z \<in> Points \<and> w \<in> Points"
+  assumes "distinct4 x y z w"
+  assumes "\<not> collinear x y z \<and> \<not> collinear x y w"
+  shows "plane_through x y z = plane_through x y w \<or> 
+        plane_through x y z \<inter> plane_through x y w = join x y"
+proof -
+  let ?P = "plane_through x y z"
+  let ?Q = "plane_through x y w"
+  have "x \<in> ?P \<and> y \<in> ?P \<and> x \<in> ?Q \<and> y \<in> ?Q" using S2a assms by auto
+  then show ?thesis using vanadium3[of x y ?P ?Q] assms S2a distinct4_def by metis
+qed
+  
 
 lemma two_point_line_in_plane0:
   fixes A B P
